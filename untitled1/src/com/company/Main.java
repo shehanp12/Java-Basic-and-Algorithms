14fe14d95baad6c1bc18aea881bffe8a3f71b21b
Shehan Prasanna <shehanp068@gmail.com>
2021-02-16 15:44:30 +0530

    multi dimentional array

---
+++ untitled1/src/com/company/Main.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import org.w3c.dom.css.CSSUnknownRule;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+     Scanner scanner = new Scanner(System.in);
+
+     int n = scanner.nextInt();
+
+     int d,q,x,y;
+
+     ArrayList[] set = new ArrayList[n];
+
+     for(int i=0;i<n;i++){
+
+         d = scanner.nextInt();
+         set[i] = new ArrayList();
+
+         for(int j=0;j<d;j++){
+             set[i].add(scanner.nextInt());
+
+         }
+     }
+//     System.out.println(set.length);
+
+        q = scanner.nextInt();
+
+     for(int z=0 ; z< q;z++){
+         x= scanner.nextInt();
+         y= scanner.nextInt();
+
+         try{
+             System.out.println(set[x-1].get(y-1));
+         }catch (Exception exception){
+             System.out.println("ERROR!");
+         }
+
+
+     }
+
+
+
+
+
+    }
+}
+
+
+
